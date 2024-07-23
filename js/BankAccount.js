261ebdb0f9db43ab9f45db95eb0489c4161660d3
aluno <lucasbrandsqueiroz@gmail.com>
2024-07-23 11:47:27 -0300

    pasta de .js

---
+++ js/BankAccount.js
@@ -0,0 +1,21 @@
+class BankAccount{
+    constructor(saldoInicial){
+        this.saldo = saldoInicial
+    }
+    depositar(valor){
+        this.saldo += valor;
+        return `novo saldo: R$ ${this.saldo}`;
+    }
+    sacar(valor){
+        if (valor > this.saldo){
+            return `saldo insuficiente!`;
+
+    }
+    this.saldo -= valor;
+    return `novo saldo: R$ ${this.saldo}`;
+
+}
+const minhaConta = new BankAccount(1000);
+console.log(minhaConta.depositar(500)); 
+console.log(minhaConta.sacar(300)); 
+console.log(minhaConta.sacar(1500)); 
